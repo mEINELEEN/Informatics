90aafbfffd2dd253d109f082f90d31a73c368941
mEINELEEN <isalimov1978@gmail.com>
2023-12-23 16:49:16 +0300

    Add work

---
+++ main.py
@@ -0,0 +1,10 @@
+text = input('Введите любую строку')
+konectext = ''
+al = 'йЙяЯыЫуУАаеЕиИоОюЮэЭaAeEyYuUiIoO'
+st = text.split()
+for i in range(0,len(st)):
+    slovo = list(st[i])
+    if slovo[0] not in al:
+        konectext += ' ' + st[i]
+print(konectext.split(','))
+
